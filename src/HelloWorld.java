9175b5844baefe91808366fdf89264b15e8ea6d1
Marek Procek <marekprocek@gmail.com>
2018-05-15 18:58:43 +0200

    Stworzenie projektu

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("ABC1");
+        System.out.print("ABC2");
+        //komentarz 1
+        System.out.print("ABC3");
+        System.out.println("asasa");
+
+    }
+}
